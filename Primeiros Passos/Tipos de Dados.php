829113074dc3aab4961f22b67fed8fbd040e9007
Victor Lima <victorlimass409@gmail.com>
2022-12-17 21:38:32 -0300

    dia 01

---
+++ Primeiros Passos/Tipos de Dados.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Aula sobre tipo de dados 
+ * 
+ * Nessa aula é explicado quais são os tipos de dados que são usados 
+ * pelo PHP.
+ * 
+ * Os tipos a serem coletados na aula:
+ * String, Boellan, Integer, Double e Array
+ * 
+ */
